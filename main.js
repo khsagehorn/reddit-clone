4dcdc5aac858d0b9a4727f02acb93ce177ec663d
khsagehorn <huntersagehorn@gmail.com>
2016-04-04 13:02:19 -0600

    worked on sorting punctions

--- main.js
+++ main.js
@@ -7,14 +7,20 @@ app.controller('PostsController', function($scope){
       id:1,
       title: 'Killer Tofu',
       author: 'Doug Funny',
-      date: '3-24-2016',    
+      date: Date.now(),    
       image: "http://www.finecooking.com/CMS/uploadedImages/Images/Cooking/Articles/Issues_61-70/fc65be071-01_xlg.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
       rating: 0,
       comments: [
         {
           user: 'Roger',
+          date: Date.now(),
           text: 'OP is a dorkus.'
+        },
+        {
+          user: 'Patty Mayonaise',
+          date: Date.now(),
+          text: 'Cut it out, Roger'
         }]
     },
 
@@ -22,7 +28,7 @@ app.controller('PostsController', function($scope){
       id:2,
       title: 'Yes, but Why?',
       author: 'Arnold',
-      date: '3-24-2016',
+      date: Date.now(),
       image: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Florida_Box_Turtle_Digon3a.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       rating: 3,
@@ -33,12 +39,13 @@ app.controller('PostsController', function($scope){
       id:3,
       title: 'Et in America, ego',
       author: 'The Judge',
-      date: '3-29-2016',
+      date: Date.now(),
       image: "http://weknowyourdreamz.com/images/desert/desert-04.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       rating: 9,
       comments: [
         {
+          date: Date.now(),
           user: "The Kid",
           text: "come on now. let's get it over with"
         }]
@@ -47,10 +54,21 @@ app.controller('PostsController', function($scope){
   ]
 
 
-  $scope.newPost = function(post) {
+  $scope.addPost = function(post) {
     $scope.post.date = Date.now();
     $scope.post.rating = 0;
     $scope.posts.push(post);
     $scope.post = {};
+    $scope.post.comments = [];
+    $scope.newPost = true;
+  }
+
+  $scope.addComment = function(comment, post_id) {
+    var id = post_id;
+    comment.date = Date.now();
+    $scope.posts[id].comments.push(comment);
+    $scope.newComment = true;
+    console.log(comment);
+    console.log(comment);
   }
 })
